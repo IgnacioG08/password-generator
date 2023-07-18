a343d7eeeadd91032b9c97ef46b6960a33459fbd
IgnacioG08 <134964013+IgnacioG08@users.noreply.github.com>
2023-07-18 14:04:06 -0700

    JS first prompt with password length & characters defined

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,25 @@
-// Assignment code here
+var numericalChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "@", "#", "$", "%", "&", "*", "_", "-", "?"];
+var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var allChar = numericalChar.concat( specialChar, lowerCaseChar, upperCaseChar );
+
+
+function generatePassword() {
+  var userChoice = window.prompt("Enter password length within the range of 8-128 characters")
+  var userChoiceNum = parseInt(userChoice, 10);
+  if (userChoiceNum < 8 || userChoiceNum > 128) {
+    window.alert("Invalid password length. Please try again.");
+    return;
+  }
+
+
+
+
+
+
+
+}
 
 
 // Get references to the #generate element
