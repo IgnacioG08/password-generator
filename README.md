039ad43862b102743ad273fcd9b27cafccc49c88
IgnacioG08 <134964013+IgnacioG08@users.noreply.github.com>
2023-07-20 15:47:37 -0700

    README created with images and notes added to code

---
+++ README.md
@@ -0,0 +1,27 @@
+# Password Generator
+
+## Description
+Password generator with several prompts utilizing many character types for optimal security. This is created with starter code and optimized using javascript to allow the user to create random passwords. User is givin the options to include upper and lower case letters, numbers, and special characters. This tool is a perfect option for individuals with the need for complex passwords up to 128 characters to protect their files.
+[Deployed Github Webpage](https://ignaciog08.github.io/password-generator/)
+
+## Visuals
+
+![alt text](./assets/images/password-generator-empty.png)
+![alt text](./assets/images/password-generator-128char.png)
+
+
+## Installation
+
+N/A
+
+## Usage
+This generator can be used to create passwords that will be incredibly strong protecting everything you may need protected. Using JavaScript we have create a user friendly interface that will be easy to navigate.
+
+
+## Credits
+
+N/A
+
+## License
+
+N/A
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,4 @@
+// Below we have all the global variables that will be referenced in this project.
 var numericalChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "@", "#", "$", "%", "&", "*", "_", "-", "?"];
 var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -5,11 +6,11 @@ var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
 var allChar = [];
 var returnValue;
 
+/*function generatePassword will create our first prompt gathering the users choice in characters and return the value of those choices
+in a randomized order*/
 function generatePassword() {
   var userChoice = window.prompt("Enter password length within the range of 8-128 characters");
   var userChoiceNum = parseInt(userChoice, 10);
- 
-
   if (isNaN(userChoiceNum) || userChoiceNum < 8 || userChoiceNum > 128) {
     window.alert("Invalid password length. Please try again.");
     generatePassword();
@@ -19,36 +20,31 @@ function generatePassword() {
     returnValue = randomize(userChoiceNum);
     window.alert("Password generated")
   } return returnValue;
-}  
+}
 
-  function charOptions () {
-    allChar = [];
+/* function charOptions will gather the types of characters that the user will be using in their password and storing them inside an array */
+function charOptions() {
+  allChar = [];
   if (window.confirm("Would you like to include uppercase letters?")) {
     allChar = allChar.concat(upperCaseChar);
-    
   }
-
   if (window.confirm("Would you like to include lowercase letters?")) {
     allChar = allChar.concat(lowerCaseChar)
-    
   }
-
   if (window.confirm("Would you like to include numbers?")) {
     allChar = allChar.concat(numericalChar)
-    
   }
-
   if (window.confirm("Would you like to include special characters?")) {
     allChar = allChar.concat(specialChar)
-  } 
   }
+}
 
-
-  function randomize (userChoiceNum) { 
-    var returnValue = ""
+/* function randomize will be responsible for looping and randomizing all the choices in the function prior to it */
+function randomize(userChoiceNum) {
+  var returnValue = ""
   for (var i = 0; i < userChoiceNum; i++) {
     returnValue += allChar[Math.floor((Math.random() * allChar.length))]
-  }  return returnValue;
+  } return returnValue;
 }
 
 
